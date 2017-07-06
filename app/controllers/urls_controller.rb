c39114c7de0d231844e8b0c8af7c3bdab06c10f2
kokchun32 <kokchun@live.co.uk>
2017-07-06 16:01:22 +0800

    ninth

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -27,6 +27,11 @@ class UrlsController < ApplicationController
     @url.destroy
     redirect_to urls_path
   end
+
+  def short
+    url = Url.find(params[:id])
+    redirect_to url.long_url    
+  end 
 end
 
 private 
